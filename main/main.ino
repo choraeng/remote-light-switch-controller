386d0485b2001a158cf3f78a54b19ab9c3b8589a
choraeng <cyh905@nate.com>
2020-11-12 22:41:10 +0900

    웹에서 각도값 수정이 가능

--- main/main.ino
+++ main/main.ino
@@ -1,22 +1,24 @@
 #include <WiFi.h>
 #include <Servo.h>
-#define OFF 0
-#define ON 1
+#define TOP 0
+#define BOT 1
+#define ON 0
+#define OFF 1
 
 // https://github.com/RoboticsBrno/ServoESP32
 // https://randomnerdtutorials.com/solved-failed-to-connect-to-esp32-timed-out-waiting-for-packet-header/
 // https://www.bneware.com/blogPost/esp32_arduino_ide
 // https://blog.naver.com/roboholic84/220583130285
-// https://arsviator.blogspot.com/2018/11/esp32-static-ip-using-static-ip-address.htmlhttps://arsviator.blogspot.com/2018/11/esp32-static-ip-using-static-ip-address.html
+// https://arsviator.blogspot.com/2018/11/esp32-static-ip-using-static-ip-address.html
 ////////// motor
-Servo servo_top, servo_bot;
+Servo servo[2];
 
 const int servopin_top = 27;
 const int servopin_bot = 25;
 
 const int rotate_idle = 45;
-const int rotate_off  = 110;
-const int rotate_on   = 00;
+int rotate[2][2] = {{0, 110},
+                    {110, 0}};
 
 const int _delay = 1000;
 
@@ -25,17 +27,14 @@ const char* ssid = "-----";
 const char* password = "-----";
 
 // set static ip
-IPAddress local_IP(0,0,0,0);
-IPAddress gateway(0,0,0,0);
+IPAddress local_IP(192, 168, 123, 105);
+IPAddress gateway(192, 168, 123, 1);
 
 IPAddress subnet(255, 255, 255, 0);
 IPAddress primaryDNS(8, 8, 8, 8);
 IPAddress secondaryDNS(8, 8, 3, 3);
 
 WiFiServer server(80);
-
-const int state_top = 0;
-const int state_bot = 0;
 
 // Variable to store the HTTP request
 String header;
@@ -44,8 +43,8 @@ void setup() {
   Serial.begin(115200);
   
   // put your setup code here, to run once:
-  servo_top.attach(servopin_top);
-  servo_bot.attach(servopin_bot);
+  servo[TOP].attach(servopin_top);
+  servo[BOT].attach(servopin_bot);
 
   if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)){
     Serial.println("STA failed to configure");
@@ -69,9 +68,9 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  servo_top.write(rotate_idle);
-  servo_bot.write(rotate_idle);
+  int isclick = 0, pos = 0, stat = 0, value = 0;
+  servo[TOP].write(rotate_idle);
+  servo[BOT].write(rotate_idle);
 
   WiFiClient client = server.available();
 
@@ -94,36 +93,40 @@ void loop() {
             client.println("Connection: close");
             client.println();
 
-            if (header.indexOf("GET /top/on") >= 0){
-              Serial.println("top on");
-              //switching(servo_top, rotate_on);
-              servo_top.write(rotate_on);
-              delay(_delay);
-              servo_top.write(rotate_idle);
-              delay(_delay);
-            }else if (header.indexOf("GET /top/off") >= 0){
-              Serial.println("top off");
-//              servo_top = switching(servo_top, rotate_off);
-              servo_top.write(rotate_off);
-              delay(_delay);
-              servo_top.write(rotate_idle);
-              delay(_delay);
-            }else if (header.indexOf("GET /bot/on") >= 0){
-              Serial.println("bot on");
-//              servo_bot = switching(servo_bot, rotate_on);
-              servo_bot.write(rotate_off);
-              delay(_delay);
-              servo_bot.write(rotate_idle);
-              delay(_delay);
-            }else if (header.indexOf("GET /bot/off") >= 0){
-              Serial.println("bot off");
-//              servo_bot = switching(servo_bot, rotate_off);
-              servo_bot.write(rotate_on);
-              delay(_delay);
-              servo_bot.write(rotate_idle);
-              delay(_delay);
+            if (header.indexOf("GET /top") >= 0){              
+              pos = TOP;
+              if (header.indexOf("GET /top/on") >= 0) {
+                stat = ON;
+                isclick = (header.indexOf("GET /top/on/set") >= 0) ? 2 : 1;
+              }else{
+                stat = OFF;
+                isclick = (header.indexOf("GET /top/off/set") >= 0) ? 2 : 1;
+              }
+            }else if (header.indexOf("GET /bot") >= 0){              
+              pos = BOT;
+              if (header.indexOf("GET /bot/on") >= 0) {
+                stat = ON;
+                isclick = (header.indexOf("GET /bot/on/set") >= 0) ? 2 : 1;
+              }else{
+                stat = OFF;
+                isclick = (header.indexOf("GET /bot/off/set") >= 0) ? 2 : 1;
+              }
             }
-            
+
+            switch (isclick){
+              case 1:
+                servo[pos].write(rotate[pos][stat]);
+                delay(_delay);
+                servo[pos].write(rotate_idle);
+                delay(_delay);
+  
+                isclick = 0;
+                break;
+             case 2:
+                rotate[pos][stat] = get_value(16 + stat);
+                isclick = 0;
+                break;
+            }
 
             // Display the HTML web page
             client.println("<!DOCTYPE html><html>");
@@ -136,20 +139,28 @@ void loop() {
             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
             client.println(".buttonon {background-color: #77878A; border: none; color: white; padding: 16px 40px;");
             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println("div{position: relative;display: inline-block;text-align: center;}");
             client.println("</style></head>");
                      
             // Web Page
             client.println("</head><body><h1>Remote Light switch Controller!!!</h1>");
+//            client.println("<div style=\"position: absolute; top: 50%; left: 50%;\">");
+            client.println("<div>");// style=\"float: left;\">");
+            
+            client.println("<div>");
             client.println("<p>Top Button</p>");
-            client.println("<div style=\"float: left;\">");
             client.println("<p><a href=\"/top/off\"><button class=\"buttonoff\">OFF</button></a></p>");
             client.println("<p><a href=\"/top/on\"><button class=\"buttonon\">ON</button></a></p>");
+            client.println("<p>ON: " + String(rotate[TOP][ON]) + " OFF: " + String(rotate[TOP][OFF]) + "</p>");
             client.println("</div>");
 
+            client.println("<div>");
             client.println("<p>Bottom Button</p>");
-            client.println("<div style=\"float: left;\">");
             client.println("<p><a href=\"/bot/off\"><button class=\"buttonoff\">OFF</button></a></p>");
             client.println("<p><a href=\"/bot/on\"><button class=\"buttonon\">ON</button></a></p>");
+            client.println("<p>ON: " + String(rotate[BOT][ON]) + " OFF: " + String(rotate[BOT][OFF]) + "</p>");
+            client.println("</div>");
+            
             client.println("</div>");
             client.println("</body></html>");     
              
@@ -172,10 +183,21 @@ void loop() {
     Serial.println("Client disconnected.");
     Serial.println("");
   }
-
-  
 }
 
+int get_value(int start_idx){
+  String temp = "";
+  int idx = start_idx;
+  for (idx; idx < start_idx+4; idx++){
+    if (header[idx] != ' '){
+      temp += header[idx];
+    }else break;
+  }
+  Serial.println("value is " + temp);
+  return temp.toInt();
+}
+
+// 왜인지 모르지만 한번만 사용 가능함 ㅜㅜ
 Servo switching(Servo servo, int rotate){
   Serial.println("start rotate.");
   servo.write(rotate);
